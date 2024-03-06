c68aa9b13c189c1466107b0ff868f3f89dcea08c
Terence Faid JABO <faidterence@outlook.com>
2024-03-06 11:42:16 +0200

    feat: Implement async thunk for assigning tickets to events with POST request

---
+++ src/features/Redux/Tickets/ticketSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+export const assignTicketToEvent = createAsyncThunk(
+  "ticket/assignTicketToEvent",
+  async ({ eventId, ticketDetails }, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`http://localhost:3000/tickets/${eventId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ticketDetails),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const ticketSlice = createSlice({
+  name: "ticket",
+  initialState: {
+    loading: false,
+    hasErrors: false,
+    tickets: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(assignTicketToEvent.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(assignTicketToEvent.fulfilled, (state, { payload }) => {
+      state.tickets.push(payload);
+      state.loading = false;
+      state.hasErrors = false;
+    });
+    builder.addCase(assignTicketToEvent.rejected, (state) => {
+      state.loading = false;
+      state.hasErrors = true;
+    });
+  },
+});
+
+export default ticketSlice.reducer;
--- src/features/Redux/index.js
+++ src/features/Redux/index.js
@@ -3,11 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSclice";
 import eventReducer from "./events/eventSlice";
 import organizerReducer from "./organizer/organizerSlice";
-
+import ticketReducer from './Tickets/ticketSlice';
 export const store = configureStore({
   reducer: {
     user: userReducer,
     event: eventReducer,
     organizer: organizerReducer,
+    ticket: ticketReducer,
   },
 });
