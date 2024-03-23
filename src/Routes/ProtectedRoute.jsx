86da48efbcc3c41a0b3a3af3c207747a7ee2bad0
Terence Faid JABO <faidterence@outlook.com>
2024-03-23 14:09:16 +0200

    feat(routes):protected

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const ProtectedRoute = ({ children, isAdmin }) => {
+const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
   const isAuthenticated = !!localStorage.getItem("token");
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/auth/signin");
-    } else if (isAdmin) {
-      navigate("/admin");
-    }
-  }, [isAuthenticated, isAdmin, navigate]);
+    if (isAuthenticated) {
+      const token = localStorage.getItem("token");
+      const decodedToken = JSON.parse(atob(token.split(".")[1]));
+      const isAdmin = decodedToken.isAdmin;
 
-  if (!isAuthenticated || isAdmin) {
-    return null; 
+      if (!isAdmin) {
+        navigate("/");
+        toast.error("You are not authorized to access this page");
+      }
+    } else {
+      navigate("/auth/signin");
+    }
+  }, [isAuthenticated, navigate]);
+
+  if (!isAuthenticated) {
+    return null;
   }
 
   return <>{children}</>;
