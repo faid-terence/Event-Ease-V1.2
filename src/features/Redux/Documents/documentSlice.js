8e33560d38da8e621fa4c650fa3d5f7dfe1cba82
Terence Faid JABO <faidterence@outlook.com>
2024-05-28 21:37:19 +0200

    bug(slice):fix document upload reducer

--- src/features/Redux/Documents/documentSlice.js
+++ src/features/Redux/Documents/documentSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
 export const uploadDocument = createAsyncThunk(
   "documentUpload/uploadDocument",
   async (document, { rejectWithValue }) => {
