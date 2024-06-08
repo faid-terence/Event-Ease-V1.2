f6d3da4953be7445b827fffe820e40bae1d52ca3
Terence Faid JABO <faidterence@outlook.com>
2024-06-09 01:42:40 +0200

    feat(dashboard):refactor styles

--- src/components/Dashboard/Users/UserTable.jsx
+++ src/components/Dashboard/Users/UserTable.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-// import UpdateUserFormModal from "./UpdateUserFormModal";
 
 const UserTable = ({ users, openModal }) => {
   const dispatch = useDispatch();
@@ -10,6 +9,7 @@ const UserTable = ({ users, openModal }) => {
 
   const handleEdit = (user) => {
     setSelectedUser(user);
+    openModal();
   };
 
   const handleDelete = (id) => {
@@ -18,19 +18,18 @@ const UserTable = ({ users, openModal }) => {
   };
 
   return (
-    <div className="overflow-x-auto px-10 mt-[150px]">
+    <div className="overflow-x-auto px-4 mt-[150px] w-full">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl font-bold">User List</h2>
-        {/* <button onClick={openModal} className="btn">
-          Add New User
-        </button> */}
       </div>
       <table className="min-w-full bg-white border border-gray-300 shadow-md rounded-md">
         <thead>
           <tr className="bg-[#EAEAEA]">
             <th className="py-3 px-4 text-left">Username</th>
-            <th className="py-3 px-4 text-left">Email</th>
-            <th className="py-3 px-4 text-left">PhoneNumber</th>
+            <th className="py-3 px-4 text-left hidden md:table-cell">Email</th>
+            <th className="py-3 px-4 text-left hidden md:table-cell">
+              Phone Number
+            </th>
             <th className="py-3 px-4 text-left">Action</th>
           </tr>
         </thead>
@@ -41,30 +40,31 @@ const UserTable = ({ users, openModal }) => {
               className="border-b border-gray-300 hover:bg-gray-100 transition-all duration-200"
             >
               <td className="py-3 px-4">{user.fullNames}</td>
-              <td className="py-3 px-4">{user.email}</td>
-              <td className="py-3 px-4">{user.phoneNumber}</td>
-              <td className="py-3 px-4">
-                <button className="mr-2">
+              <td className="py-3 px-4 hidden md:table-cell">{user.email}</td>
+              <td className="py-3 px-4 hidden md:table-cell">
+                {user.phoneNumber}
+              </td>
+              <td className="py-3 px-4 flex items-center">
+                <button className="mr-2 text-gray-600 hover:text-gray-800 transition-colors duration-300">
                   <FaEye />
                 </button>
-                <button className="mr-2" onClick={() => handleEdit(user)}>
-                  <FaEdit color="blue" />
+                <button
+                  className="mr-2 text-blue-500 hover:text-blue-600 transition-colors duration-300"
+                  onClick={() => handleEdit(user)}
+                >
+                  <FaEdit />
                 </button>
-                <button onClick={() => handleDelete(user.id)}>
-                  <FaTrash color="red" />
+                <button
+                  className="text-red-500 hover:text-red-600 transition-colors duration-300"
+                  onClick={() => handleDelete(user.id)}
+                >
+                  <FaTrash />
                 </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      {/* {selectedUser && (
-        <UpdateUserFormModal
-          isOpen={true}
-          onClose={() => setSelectedUser(null)}
-          userData={selectedUser}
-        />
-      )} */}
     </div>
   );
 };
